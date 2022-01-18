25881fbbf0560b0cd295e40a7bcdf4e89471bc3e
ayoub3bidi <mrayoubabidi@gmail.com>
2022-01-18 17:35:42 +0100

    ⚡️ Fetching Data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,17 @@
 
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch ('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    console.log(data)
+  },
 }
 </script>
