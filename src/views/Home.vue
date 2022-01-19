0450eebaacfea1f8ffb202d00d440c5d63553a40
ayoub3bidi <mrayoubabidi@gmail.com>
2022-01-19 10:18:21 +0100

    ✅ Adding DataTitle component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main v-if="!loading">
-    Show Data
+    <DataTitle :text="title" :dataDate="dataDate"/>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -10,9 +10,13 @@
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
 
 export default {
   name: 'Home',
+  components: {
+    DataTitle
+  },
   data() {
     return {
       loading: true,
