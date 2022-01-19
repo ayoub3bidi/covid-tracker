ae7f69c8b87221adc692692ce13183cedd8893d8
ayoub3bidi <mrayoubabidi@gmail.com>
2022-01-19 22:52:11 +0100

    💄 Re-styling DataTitle component

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="text-center">
-        <h2 class="text-3xl font-bold">{{text}}</h2>
+    <div class="text-center bg-blue-600 text-white p-4 mb-10">
+        <h2 class="text-3xl md:text-5xl font-bold mb-3">{{text}}</h2>
         <div class="text-2xl mt-4 mb-10">
             {{ timestamp }}
         </div>
@@ -15,7 +15,7 @@ export default {
     props: ['text', 'dataDate'],
     computed: {
         timestamp: function() {
-            return moment(this.dataDate).format('MMMM D YYYY, h:mm:ss a')
+            return moment(this.dataDate).format('MMMM D YYYY')+ " | " + moment(this.dataDate).format('h:mm:ss a')
         }
     },
 }
