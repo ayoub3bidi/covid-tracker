6edc4dd73114667cdbdfeb509b5de306dfcbfafa
ayoub3bidi <mrayoubabidi@gmail.com>
2022-01-20 10:49:13 +0100

    ✅ Clearing Country Data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,14 @@
     <DataBoxes :stats="stats"/>
 
     <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -44,7 +52,14 @@ export default {
     getCountryData(country) {
       this.stats = country
       this.title = country.Country
-    }
+    },
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
+    },
   },
   async created() {
     const data = await this.fetchCovidData()
